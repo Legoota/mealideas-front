348b884c1988ea325ff9b5dca52b439a144472fe
Léo K <leokrebs68@gmail.com>
2024-06-24 08:34:29 +0200

    Adding calendar meal dates

--- src/components/CalendarComponent.vue
+++ src/components/CalendarComponent.vue
@@ -4,6 +4,7 @@
       <v-calendar
         v-model="value"
         view-mode="month"
+        :events="mealDates"
         :weekdays="[1, 2, 3, 4, 5, 6, 0]"
       ></v-calendar>
     </v-responsive>
@@ -11,6 +12,35 @@
 </template>
 
 <script setup lang="ts">
+import { Meal } from '@/models/meal';
+import axios from "axios";
+
   const value: Ref<Date[]> = ref([new Date()]);
+  const meals : Ref<Meal[]> = ref([]);
+
+  onMounted(async () => {
+  await axios
+    .get('http://localhost:8080/api/meals')
+    .then(response => {
+      meals.value = response.data
+    })
+    .catch(err => {
+        console.error(err);
+      });
+  });
+
+  const mealDates = computed(() => {
+    if(!meals || !meals.value || meals.value.length < 1) return [];
+    let mealsToAdd = meals.value.filter(m => m.date_lastuse);
+    return mealsToAdd.map(m => { 
+      return {
+        title: m.name,
+        start: new Date(m.date_lastuse || new Date()),
+        end: new Date(m.date_lastuse || new Date()),
+        allDay: true,
+        color: ["blue", "green", "red"][m.type]
+      }
+    });
+  });
 
 </script>
